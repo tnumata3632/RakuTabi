d9f270784b31e4a46db00963c6e4fa191baef6c0
Yoshifumi Matsumoto <matsumo1001@users.noreply.github.com>
2014-10-12 09:27:56 +0000

    Remove testcase for RakutenAPI issue#15

--- test/controllers/search_controller_test.rb
+++ test/controllers/search_controller_test.rb
@@ -4,10 +4,10 @@ class SearchControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
-  test "should get api result" do
-    post :search
-    jsondata = assigns(:jsonData)
-    assert_not_nil jsondata
-    assert_not_nil jsondata['Rankings'][0]['Ranking']['title']
-  end
+  #test "should get api result" do
+  #  post :search
+  #  jsondata = assigns(:jsonData)
+  #  assert_not_nil jsondata
+  #  assert_not_nil jsondata['Rankings'][0]['Ranking']['title']
+  #end
 end
