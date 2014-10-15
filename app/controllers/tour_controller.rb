b42efe78afe711d315008f8dc0522243a6301ad7
Numata <t.numata.3632@gmail.com>
2014-10-15 17:36:58 +0000

    Add issue#27 選択した画像に応じた結果を表示

--- app/controllers/tour_controller.rb
+++ app/controllers/tour_controller.rb
@@ -4,7 +4,14 @@ class TourController < ApplicationController
 
   def search
     tour = AbroadTour.new
-    keyword = "世界遺産 トルコ フィンランド"
+    case params[:imgid]
+    when "town"
+      keyword = "街 ニューヨーク"
+    when "sea"
+      keyword = "海 ビーチ"
+    else
+      keyword = "世界遺産 トルコ フィンランド"
+    end
     dept = "TYO"
     @jsonData = tour.get_tours(keyword, dept)
   end
