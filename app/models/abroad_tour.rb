5b700056bec50096eae60974fd8c8ca6b1e87c8a
Yoshifumi Matsumoto <matsumo1001@users.noreply.github.com>
2014-10-23 11:56:14 +0900

    Improve search logic #37

--- app/models/abroad_tour.rb
+++ app/models/abroad_tour.rb
@@ -1,12 +1,30 @@
 class AbroadTour
   API_URL = "http://webservice.recruit.co.jp/ab-road/tour/v1/"
   FORMAT = "json"
+  COUNT = 100
 
   def initialize
     @appid = ENV["ABROAD_APPID"]
   end
 
-  def get_tours(keyword, dept)
+  def get_tours(keyword, dept="TYO")
+    allTours = get_tours_all(keyword, dept, 1, COUNT)
+    numOfTours = allTours["results_returned"]
+    p numOfTours
+    hash = Hash.new
+    allTours["results"]["tour"].each{|item|
+      key = item['city_summary']
+      unless hash.has_key?(key)
+        hash.store(key, item)
+        if hash.length > 14
+          break 
+        end
+      end
+    }
+    return hash.values
+  end
+
+  def get_tours_all(keyword, dept="TYO", start=1, count=10)
     httpClient = HTTPClient.new
     jsonData = nil
     begin
@@ -14,7 +32,9 @@ class AbroadTour
         "key" => @appid,
         "keyword" => keyword,
         "dept" => dept,
-        "format" => FORMAT
+        "format" => FORMAT,
+        "start" => start,
+        "count" => count
       })
       jsonData = JSON.parse data
       Rails.logger.debug(jsonData.inspect)
