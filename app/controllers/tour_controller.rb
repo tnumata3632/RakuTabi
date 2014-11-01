bb7e40fe395696dde72da5411bd6a9cb582c1566
Yoshifumi Matsumoto <matsumo1001@users.noreply.github.com>
2014-11-01 14:22:56 +0900

    Modify get_tour method #49

--- app/controllers/tour_controller.rb
+++ app/controllers/tour_controller.rb
@@ -18,7 +18,7 @@ class TourController < ApplicationController
       keyword = "世界遺産 トルコ フィンランド"
     end
     dept = "TYO"
-    @tours = tour.get_tours(keyword, dept)
+    @tours = tour.get_tours_by_keyword(keyword, dept)
     p "dept    = " + dept
     p "keyword = " + keyword
   end
