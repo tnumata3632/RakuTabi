2c16587b7477f33a77925415be96013e1ad2d19d
Numata <t.numata.3632@gmail.com>
2014-11-05 15:00:52 +0000

    Add dummy method of get_tours_all()

--- app/models/abroad_tour.rb
+++ app/models/abroad_tour.rb
@@ -9,13 +9,13 @@ class AbroadTour
 
   def get_tours(keyword, dept="TYO", start=1, count=10)
     # 最初の100件取得
-    tours = get_tours_all(keyword, dept, 1, COUNT)
+    tours = get_tours_all_m(keyword, dept, 1, COUNT)
     resAvailable = tours["results"]["results_available"].to_i
     resReturned = tours["results"]["results_returned"].to_i
     tourArray = tours["results"]["tour"]
     # 検索結果が100件以上の場合、最後の100件を取得し最初の100件とマージする
     if resAvailable > resReturned
-      toursTail = get_tours_all(keyword, dept, resAvailable>COUNT*2 ? resAvailable-COUNT : COUNT+1, COUNT)
+      toursTail = get_tours_all_m(keyword, dept, resAvailable>COUNT*2 ? resAvailable-COUNT : COUNT+1, COUNT)
       tourArray.concat(toursTail["results"]["tour"])
     end
     # 結果から、同一の宿泊地要約のツアーをhashで除外する
@@ -53,6 +53,69 @@ class AbroadTour
       rescue HTTPClient::BadResponseError => e
       rescue HTTPClient::TimeoutError => e
     end
+    return jsonData
+  end 
+
+  def get_tours_all_m(keyword, dept="TYO", start=1, count=10)
+    jsonData = {"results" => {
+      "results_available" => 2,
+      "results_returned" => 2,
+      "tour" => [
+      {
+        "dest" => {
+          "lat" => "16.0550517622",
+          "lng" => "108.2235717773",
+          "name" => "ダナン"
+        },
+        "img" => [
+          {
+            "l"=>"http://www.ab-road.net/tour_img/size_l/1756578003_393400_l.jpg",
+            "caption"=>"ランタン／イメージ"
+          },
+        ],
+        "urls" => {
+          "pc"=>"http://google.co.jp/"
+        },
+        "id" => "AD04H0SV",
+        "city_summary" => "ダナン",
+        "title" => "ベトナム航空で行く 世界遺産の街　ダナン乗継便5日間！...",
+      },
+      {
+        "dest" => {
+          "lat" => "14.0283490000",
+          "lng" => "99.5299530000",
+          "name" => "カンチャナブリ"
+        },
+        "img" => [
+          {
+            "l"=>"http://www.ab-road.net/tour_img/size_l/3196435002_415182_l.jpg",
+            "caption"=>"アユタヤ　世界遺産祭り"
+          },
+        ],
+        "urls" => {
+          "pc"=>"http://google.co.jp/"
+        },
+        "id" => "AC862685",
+        "city_summary" => "バンコク、カンチャナブリ",
+        "title" => "2014年　アユタヤ世界遺産祭り2014年を見学♪♪　■　キャセイ航空利用 ...",
+      },
+      {
+        "dest" => {
+          "lat" => "45.4341170000",
+          "lng" => "12.3390200000",
+          "name" => "ベネチア"
+        },
+        "img" => [
+        ],
+        "urls" => {
+          "pc"=>"http://google.co.jp/"
+        },
+        "id" => "AD04KMDK",
+        "city_summary" => "ベネチア",
+        "title" => "【イタリアお祭りツアー】 ★世界三大祭り！仮面カーニバルへ行く ...",
+      },
+      ]
+    }}
     return jsonData
   end 
 end
